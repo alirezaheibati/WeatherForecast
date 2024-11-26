cb2f1155e59d2d150f79edc263ed1fbe0b7cb220
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-26 20:17:18 +0330

    feat (src/js/models/cityWeatherModel.js): implement loadCityWeatherByCoords method

--- src/js/models/cityWeatherModel.js
+++ src/js/models/cityWeatherModel.js
@@ -1,3 +1,4 @@
+import { API_KEY, API_URL } from "../config";
 /**
  * Represents the weather information model for a city.
  */
@@ -7,5 +8,23 @@ class CityWeatherModel {
      * Stores weather information for a city.
      */
     this.weatherInfo = {};
+  }
+  /**
+   * Loads weather data for a specific city using latitude and longitude.
+   *
+   * @param {number} lat - The latitude of the city.
+   * @param {number} lon - The longitude of the city.
+   * @returns {Promise<void>} - A promise that resolves when the data is fetched and set.
+   */
+  async loadCityWeatherByCoords(lat, lon) {
+    try {
+      const data = await getJSON(
+        `${API_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`
+      );
+
+      this.weatherInfo = data;
+    } catch (err) {
+      throw err;
+    }
   }
 }
