82749a8c8ca9a451c23702e974922d71416216eb
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-26 16:22:52 +0330

    feat (tailwind.config.js): Add screen breakpoints to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./index.html", "./src/**/*/{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   plugins: [],
 };
