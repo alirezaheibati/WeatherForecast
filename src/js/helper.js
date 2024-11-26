864e6a38e0d615ec2b1606a721b122ac6a48f129
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-26 19:53:52 +0330

    feat (src/js/helper.js): Add getJSON function to fetch JSON data

--- src/js/helper.js
+++ src/js/helper.js
@@ -16,3 +16,21 @@ const timeout = function (second) {
     }, second * 1000);
   });
 };
+/**
+ * Fetches JSON data from the provided URL.
+ * Uses the Fetch API to make an asynchronous request.
+ *
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Promise<Object>} - A promise that resolves to the JSON data.
+ * @throws {Error} - Throws an error if the request fails or if the response is not OK.
+ */
+export const getJSON = async function (url) {
+  try {
+    const res = await Promise.race([fetch(url), timeout(7)]);
+    const data = await res.json();
+    if (!res.ok) throw new Error("someThing went wrong");
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
