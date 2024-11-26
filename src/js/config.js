dc04daf922be1ac80bbfdc31ade8a9289f77b7a8
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-26 16:51:09 +0330

    feat (src/js/config.js): create config file with API_KEY and API_URL constants

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+/**
+ * The API key for accessing the openweathermap API.
+ */
+export const API_KEY = "b26118e0c4f7d4d46a45254cdf447207";
+/**
+ * The base URL for the openweathermap API.
+ * All API requests will be made to this endpoint.
+ */
+export const API_URL = "https://api.openweathermap.org/data/2.5/forecast?";
