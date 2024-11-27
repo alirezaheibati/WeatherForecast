bf8242f7e918bab164557d651346fb0975c5478a
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-27 08:30:50 +0330

    feat (src/js/views/): implement View class as base class so other view classes can extend from

---
+++ src/js/views/View.js
@@ -0,0 +1,12 @@
+/**
+ * Represents the base view class.
+ * This class serves as a parent class for other view classes to extend from.
+ */
+export default class View {
+  /**
+   * Holds data for the view.
+   * @type {*}
+   * @protected
+   */
+  _data;
+}
