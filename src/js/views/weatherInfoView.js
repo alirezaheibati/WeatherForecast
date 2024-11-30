fcc5c0ee8faed308598055e14b79d7b483a02300
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-30 14:23:14 +0330

    feat (src/js/views/weatherInfoView.js): implement _generateMarkup method for weatherInfoView class

--- src/js/views/weatherInfoView.js
+++ src/js/views/weatherInfoView.js
@@ -10,4 +10,22 @@ class WeatherInfoView extends View {
    * @protected
    */
   _parentElement = document.getElementById("weather-info");
+  /**
+   * Generates the markup for the main weather information.
+   *
+   * @returns {string} The HTML markup for the weather information.
+   * @protected
+   */
+  _generateMarkup() {
+    return `
+      <p class="text-9xl text-center">${this._data.main.temp}&deg;</p>
+            <div class="flex justify-center items-center gap-2">
+              <img src="https://openweathermap.org/img/wn/${this._data.weather[0].icon}@2x.png" alt="${this._data.weather[0].main}"> 
+              <p class="text-4xl">
+              ${this._data.weather[0].main}
+              </p>
+            </div>
+      `;
+  }
 }
+export const weatherInfoView = new WeatherInfoView();
