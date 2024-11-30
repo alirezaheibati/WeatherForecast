727865aeff87297463090b2a29b54c072492acd0
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-30 14:23:50 +0330

    feat (src/js/views/weatherInfoView.js): implement _generateMarkup method for locationInfoView class

--- src/js/views/locationInfoView.js
+++ src/js/views/locationInfoView.js
@@ -10,4 +10,26 @@ class LocationInfoView extends View {
    * @protected
    */
   _parentElement = document.getElementById("location-info");
+  _formatDate() {
+    const date = new Date();
+    const options = { weekday: "long", day: "numeric", month: "long" };
+    return date.toLocaleDateString("en-US", options);
+  }
+  /**
+   * Generates the markup for information about city.
+   *
+   * @returns {string} The HTML markup for the city information.
+   * @protected
+   */
+  _generateMarkup() {
+    return `
+    <p class="mt-12 text-3xl">
+              <i class="fa-solid fa-location-dot"></i> ${this._data.country}, ${
+      this._data.name
+    }
+            </p>
+            <p>${this._formatDate()}</p>
+    `;
+  }
 }
+export const locationInfoView = new LocationInfoView();
