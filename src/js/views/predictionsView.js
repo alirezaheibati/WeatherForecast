7a57c8bd353f207a94d07ac3fa6877cc0c7dc848
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-11-30 10:44:31 +0330

    feat (src/js/views/predictionsView.js): implement _generateMarkup method for predictionsView class

--- src/js/views/predictionsView.js
+++ src/js/views/predictionsView.js
@@ -10,4 +10,32 @@ class PredictionsView extends View {
    * @protected
    */
   _parentElement = document.getElementById("predictions-view");
+  /**
+   * Generates the markup for the next 6 weather predictions each 3 hours.
+   *
+   * @returns {string} The HTML markup for the weather highlights.
+   * @protected
+   */
+  _generateMarkup() {
+    const predictions = [];
+
+    for (let i = 1; i < 7; i++) {
+      predictions.push(`
+                      <div
+                class="bg-white w-[48%] xl:w-auto xl:flex-grow p-2 mb-2 rounded-lg text-center"
+              >
+                <p>${this._data.list[i].weather[0].description}</p>
+                <img src="https://openweathermap.org/img/wn/${
+                  this._data.list[i].weather[0].icon
+                }.png" alt="${
+        this._data.list[i].weather[0].main
+      }" class="block mx-auto">
+                <p>temp: ${this._data.list[i].main.temp}&deg;</p>
+                <p>${this._data.list[i].dt_txt.slice(5, 16)}</p>
+              </div>
+        `);
+    }
+    return predictions.join(" ");
+  }
 }
+export const predictionsView = new PredictionsView();
