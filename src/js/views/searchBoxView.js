2ad323b88eef8aa85bfee2a3ccc5861fd526882d
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-01 09:27:53 +0330

    feat (src/js/views/searchBoxView.js): implement addHandlerSearchCity method for searchBoxView class

--- src/js/views/searchBoxView.js
+++ src/js/views/searchBoxView.js
@@ -8,5 +8,26 @@ class SearchBoxView {
    * @type {HTMLElement}
    */
   _parentElement = document.getElementById("city-selector-container");
+  /**
+   * Create a search box view instance.
+   * Automatically binds the search city handler.
+   */
+  constructor() {
+    this.addHandlerSearchCity();
+  }
+  /**
+   * Add an event listener to handle search city interactions.
+   *
+   * This method listens for clicks on the parent element and
+   * checks if the close button is clicked. If so, it adds a class
+   * to the parent element to hide the search box.
+   */
+  addHandlerSearchCity() {
+    this._parentElement.addEventListener("click", (e) => {
+      const closeBtn = e.target.closest("#close-search-box");
+      if (!closeBtn) return;
+      this._parentElement.classList.add("-translate-x-full");
+    });
+  }
 }
 export const searchBoxView = new SearchBoxView();
