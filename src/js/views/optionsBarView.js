ad534d17f1fd156e8cd885361ccac05d26496b88
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-01 11:11:10 +0330

    feat (src/js/views/optionsBarView.js): implement OptionsBarView class

---
+++ src/js/views/optionsBarView.js
@@ -0,0 +1,12 @@
+/**
+ * Represents the view for a city page option bar.
+ */
+class OptionsBarView {
+  /**
+   * The parent element in the DOM where the weather highlights will be rendered.
+   * @type {HTMLElement}
+   * @protected
+   */
+  _parentElement = document.getElementById("menu-bar-container");
+}
+export const optionsBarView = new OptionsBarView();
