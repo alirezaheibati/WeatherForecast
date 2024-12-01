bbd7338e2e245430e923fb952505298b85425c4b
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-01 11:12:30 +0330

    feat (src/js/views/optionsBarView.js): implement addHandlerSearchCity method fofr OptionsBarView class

--- src/js/views/optionsBarView.js
+++ src/js/views/optionsBarView.js
@@ -8,5 +8,20 @@ class OptionsBarView {
    * @protected
    */
   _parentElement = document.getElementById("menu-bar-container");
+  /**
+   * Add an event listener to handle search city interactions.
+   *
+   * This method listens for clicks on the parent element and
+   * removes the class that hides the city selector container.
+   */
+  addHandlerSearchCity() {
+    this._parentElement.addEventListener("click", (e) => {
+      const seaerchContainer = document.getElementById(
+        "city-selector-container"
+      );
+      if (!seaerchContainer) return;
+      seaerchContainer.classList.remove("-translate-x-full");
+    });
+  }
 }
 export const optionsBarView = new OptionsBarView();
