c831f049f4f13d25a451a49d17556c1b7e25ed74
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-01 09:27:07 +0330

    feat (src/js/views/searchBoxView.js): implement searchBoxView class

---
+++ src/js/views/searchBoxView.js
@@ -0,0 +1,12 @@
+/**
+ * Class representing the search box view.
+ */
+class SearchBoxView {
+  /**
+   * The parent element that contains the city selector.
+   * @private
+   * @type {HTMLElement}
+   */
+  _parentElement = document.getElementById("city-selector-container");
+}
+export const searchBoxView = new SearchBoxView();
