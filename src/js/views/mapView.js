86b497740c620beaaa1a11dda532421d2b6d7bd1
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-02 12:22:34 +0330

    feat (src/js/views/mapView.js): implement initiateMap method for  mapView class

--- src/js/views/mapView.js
+++ src/js/views/mapView.js
@@ -1,3 +1,4 @@
+import L from "leaflet";
 /**
  * Class representing the leaflet map view.
  */
@@ -8,4 +9,21 @@ class MapView {
    * @type {HTMLElement}
    */
   _parentElement = document.getElementById("map");
+  constructor() {
+    this.initiateMap();
+  }
+  /**
+   * initiateMap Method
+   * Sets up the Leaflet map and its tile layer.
+   */
+  initiateMap() {
+    // Create the Leaflet map and set the view to a specific location and zoom level
+    this.map = L.map(this._parentElement).setView([35.72, 51.38], 11);
+    // Add OpenStreetMap tile layer to the map
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+  }
 }
+export const mapView = new MapView();
