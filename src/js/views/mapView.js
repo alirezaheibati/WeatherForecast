aa52bcdb1a99bbf8e19a7d42ec8da44f98f0ae1e
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-02 10:40:26 +0330

    feat (src/js/views/mapView.js): create mapView class

---
+++ src/js/views/mapView.js
@@ -0,0 +1,11 @@
+/**
+ * Class representing the leaflet map view.
+ */
+class MapView {
+  /**
+   * The parent element where leaflet map will render.
+   * @private
+   * @type {HTMLElement}
+   */
+  _parentElement = document.getElementById("map");
+}
