35fd8b4f84de58f9b60210f73aeac16865ab6dd4
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-04 11:05:43 +0330

    feat (src/js/views/containerViwe.js): implement addHandlerToWindowResize method to ContainerView class

--- src/js/views/containerViwe.js
+++ src/js/views/containerViwe.js
@@ -8,5 +8,18 @@ class ContainerView {
    * @protected
    */
   _parentElement = document.getElementById("main-container");
+  /**
+   * Adds an event listener for the window resize event.
+   * The handler is only called when the window width is greater than 768px.
+   *
+   * @param {function} handler - The function to call when the window is resized.
+   */
+  addHandlerToWindowResize(handler) {
+    window.addEventListener("resize", (e) => {
+      if (e.target.innerWidth > 768) {
+        handler();
+      }
+    });
+  }
 }
 export const containerView = new ContainerView();
