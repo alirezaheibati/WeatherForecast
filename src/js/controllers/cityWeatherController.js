224c40cde6398a3251971becd079366e76cc43cc
alirezaheibati <123858715+alirezaheibati@users.noreply.github.com>
2024-12-04 12:20:05 +0330

    feat (src/js/controller): create cityWeatherController class

---
+++ src/js/controllers/cityWeatherController.js
@@ -0,0 +1,11 @@
+/**
+ * CityWeatherController class that manages the interaction between the model and views.
+ * It handles user interactions, fetches weather data, and updates the views accordingly.
+ */
+export class CityWeatherController {
+  /**
+   * Constructor for the CityWeatherController class.
+   * Initializes the model and views, and sets up event handlers for user interactions.
+   */
+  constructor() {}
+}
